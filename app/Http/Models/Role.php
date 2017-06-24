f5795edc7f8b8354a0a57195ce79670897053b40
mrlifang <1152730631@qq.com>
2017-06-24 16:29:31 +0800

    建立Role Manager 1对多关系 [示例]

--- app/Http/Models/Role.php
+++ app/Http/Models/Role.php
@@ -11,4 +11,47 @@ class Role extends Model
     protected $primaryKey = 'role_id';
 
     protected $fillable = ['role_name','role_auth_ids','role_auth_ac'];
+
+    /*
+     * [建立Role Manager 1对多关系]
+     * [使用关系]
+     * ① 属性方式
+     * $managers = role模型对象->manager
+     * 一个角色对应很多个管理员
+     * foreach($managers as $v){
+     *
+     * $v -> mg_name;//获得model模型具体信息
+     * $v -> mg_phone;
+     *
+     * }
+     *
+     *
+     * ② with()方式
+     * $info = Role::with(关系方法名称)->get();
+     * $info = Role::with('manager')->get(); 返回一个角色集合
+     * foreach($info as $v){
+     *     $v -> role_name
+     *     $v -> manager;//获得一个当前角色对应的管理员集合
+     *      foreach($v->manager as $m){
+     *
+     *          $m -> username //获取当前角色对应的管理员集合中
+     *                         //的一个管理员名称
+     *      }
+     * }
+     *
+     *
+     *[结构图]
+     * Collection集合
+     *         单元role
+     *              调用角色的属性
+     *          managers成员(Collection集合)
+     *              遍历managers
+     *                  manager对象->mg_id/username
+     *
+     */
+
+    function manager(){
+        return $this->hasMany('App\Http\Models\Manager','mg_role_ids','mg_role');
+    }
+
 }
